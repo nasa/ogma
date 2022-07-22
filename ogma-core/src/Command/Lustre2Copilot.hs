ac527c3534c3c33a78b5cf82623e5ef5d6705275
Ivan Perez <ivan.perezdominguez@nasa.gov>
2022-07-22 14:28:47 +0000

    WIP: Adapt lustre2Copilot command to match new architecture.
    [ci skip]

--- ogma-core/src/Command/Lustre2Copilot.hs
+++ ogma-core/src/Command/Lustre2Copilot.hs
@@ -30,21 +30,27 @@
 --
 -- | Transform a Lustre file into a Copilot specification.
 module Command.Lustre2Copilot
-    ( lustre2Copilot )
+    ( lustre2Copilot
+    , ErrorCode
+    )
   where
 
 -- External imports
 import System.Exit ( ExitCode (ExitFailure), exitWith )
 import System.IO   ( hPutStrLn, stderr )
 
--- Internal imports: Auxiliary
+-- Internal imports: auxiliary
 import Data.String.Extra as S ( safeReadFile )
+
+-- Internal imports: auxiliary
+import Command.Result ( Result (..) )
+import Data.Location  ( Location (..) )
 
 -- Internal imports: Lustre parsing and AST
 import qualified Language.Lustre.AbsLustre as L ( Program )
 import qualified Language.Lustre.ParLustre as L ( myLexer, pProgram )
 
--- Internal imports: Transformation of Lustre to Copilot
+-- Internal imports: transformation of Lustre to Copilot
 import qualified Language.Trans.Lustre2Copilot as L ( lustre2Copilot )
 
 -- | Print the contents of a Copilot module that implements monitors for
@@ -53,7 +59,8 @@ import qualified Language.Trans.Lustre2Copilot as L ( lustre2Copilot )
 -- PRE: The file given is readable, contains a valid Lustre file., none of the
 -- variables used in Lustre clash with any identifiers that exist in Copilot.
 -- All identifiers used are valid C99 identifiers.
-lustre2Copilot :: FilePath -> IO ()
+lustre2Copilot :: FilePath
+               -> IO (Result ErrorCode)
 lustre2Copilot fp = do
 
   -- All of the following operations use Either to return error messages. The
@@ -64,18 +71,30 @@ lustre2Copilot fp = do
   -- before the first side effects (output file creation), or the file may be
   -- (over) written even when processing fails.
   lustre <- parseLustreProgram fp
-  case L.lustre2Copilot =<< lustre of
-    Left msg -> do hPutStrLn stderr ("ogma: " ++ fp ++ ": error: " ++ msg)
-                   exitWith (ExitFailure 1)
-    Right t  -> putStrLn t
 
--- | Parse a Lustre file.
+  case L.lustre2Copilot =<< lustre of
+    Right spec  -> putStrLn spec >> return Success
+    Left msg    -> return $ Error ecLustreError msg (LocationFile fp)
+
+    -- | Parse a Lustre file.
+    --
+    -- Returns a 'Left' with an error message if the file does not have the
+    -- correct format.
+    --
+    -- Throws an exception if the file cannot be read.
+    parseLustreProgram :: FilePath -> IO (Either String L.Program)
+    parseLustreProgram fp = do
+      content <- S.safeReadFile fp
+      return $ L.pProgram . L.myLexer =<< content
+
+-- * Error codes
+
+-- | Encoding of reasons why the command can fail.
 --
--- Returns a 'Left' with an error message if the file does not have the correct
--- format.
---
--- Throws an exception if the file cannot be read.
-parseLustreProgram :: FilePath -> IO (Either String L.Program)
-parseLustreProgram fp = do
-  content <- S.safeReadFile fp
-  return $ L.pProgram . L.myLexer =<< content
+-- The error code used is 1 for user error.
+type ErrorCode = Int
+
+-- | Error: the Lustre file cannot be read due to the file being unreadable or
+-- the format being incorrect.
+ecLustreError :: ErrorCode
+ecLustreError = 1
