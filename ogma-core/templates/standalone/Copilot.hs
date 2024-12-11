1ee00eddd1a2ba2a81c297f0ae81698788749687
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-12-11 11:41:31 +0000

    ogma-core: Use template expansion to generate standalone monitoring application. Refs #189.
    The standalone backend uses a fixed template to generate the Copilot
    monitor. That template does not fit all use cases, so we are finding
    users heavily modifying the output (which is hard to keep up with when
    there are changes), or not using ogma altogether for that reason.
    
    This commit modifies the ogma-core standalone command to use mustache to
    generate the Copilot monitor via a template and variable expansion. We
    introduce a new template that uses variables, and we modify the cabal
    file to include the files that make up the default template as data
    files that are copied over during installation.
    
    To be able to generate the files, we need to introduce a target
    directory option for the standalone command.

---
+++ ogma-core/templates/standalone/Copilot.hs
@@ -0,0 +1,40 @@
+import           Copilot.Compile.C99
+import           Copilot.Language          hiding (prop)
+import           Copilot.Language.Prelude
+import           Copilot.Library.LTL       (next)
+import           Copilot.Library.MTL       hiding (since, alwaysBeen, trigger)
+import           Copilot.Library.PTLTL     (since, previous, alwaysBeen)
+import qualified Copilot.Library.PTLTL     as PTLTL
+import qualified Copilot.Library.MTL       as MTL
+import           Language.Copilot          (reify)
+import           Prelude                   hiding ((&&), (||), (++), (<=), (>=), (<), (>), (==), (/=), not)
+
+{{{externs}}}
+{{{internals}}}
+{{{reqs}}}
+
+-- | Clock that increases in one-unit steps.
+clock :: Stream Int64
+clock = [0] ++ (clock + 1)
+
+-- | First Time Point
+ftp :: Stream Bool
+ftp = [True] ++ false
+
+pre :: Stream Bool -> Stream Bool
+pre = ([False] ++)
+
+tpre :: Stream Bool -> Stream Bool
+tpre = ([True] ++)
+
+notPreviousNot :: Stream Bool -> Stream Bool
+notPreviousNot = not . PTLTL.previous . not
+
+-- | Complete specification. Calls C handler functions when properties are
+-- violated.
+spec :: Spec
+spec = do
+{{{triggers}}}
+
+main :: IO ()
+main = reify spec >>= compile "{{{specName}}}"
