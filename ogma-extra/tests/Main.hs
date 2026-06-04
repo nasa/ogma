a6d8072d3820799fb7cce6c994c38548b677ebe4
hammadxcm <hammadkhanxcm@gmail.com>
2026-06-04 15:16:13 +0500

    ogma-extra: Add unit tests for copyTemplate. Refs #390.
    The function System.Directory.Extra.copyTemplate is not covered by the
    unit tests of ogma-extra.
    
    This commit adds unit tests that check that copyTemplate expands a
    valid template correctly, and that it reports the expected
    CopyTemplateError when the template directory does not exist, when a
    template file is not a valid UTF-8 text file, and when a template file
    contains invalid template syntax.

--- ogma-extra/tests/Main.hs
+++ ogma-extra/tests/Main.hs
@@ -15,17 +15,30 @@
 -- License for the specific language governing permissions and limitations
 -- under the License.
 --
+{-# LANGUAGE OverloadedStrings #-}
 -- | Test ogma-extra internals.
 module Main where
 
 -- External imports
-import Data.Either                          ( isLeft, isRight )
-import Test.Framework                       ( Test, defaultMainWithOpts )
-import Test.Framework.Providers.QuickCheck2 ( testProperty )
-import Test.QuickCheck                      ( Property, (==>), Fun (Fun) )
+import           Control.Exception                    ( try )
+import           Data.Aeson                           ( object, (.=) )
+import qualified Data.ByteString                      as BS
+import           Data.Either                          ( isLeft, isRight )
+import           System.Directory                     ( createDirectoryIfMissing,
+                                                        getTemporaryDirectory,
+                                                        removePathForcibly )
+import           System.FilePath                      ( (</>) )
+import           Test.Framework                       ( Test,
+                                                        defaultMainWithOpts )
+import           Test.Framework.Providers.HUnit       ( testCase )
+import           Test.Framework.Providers.QuickCheck2 ( testProperty )
+import           Test.HUnit                           ( assertBool )
+import           Test.QuickCheck                      ( Fun (Fun), Property,
+                                                        (==>) )
 
 -- Internal imports
-import Data.List.Extra ( headEither, toHead, toTail )
+import Data.List.Extra        ( headEither, toHead, toTail )
+import System.Directory.Extra ( CopyTemplateError (..), copyTemplate )
 
 -- | Run all unit tests on Ogma's core.
 main :: IO ()
@@ -45,6 +58,14 @@ tests =
   , testProperty "Data.List.Extra.toTail (length)"             propToTailSameLength
   , testProperty "Data.List.Extra.headEither (Right case)"     propHeadEitherRight
   , testProperty "Data.List.Extra.headEither (Left case)"      propHeadEitherLeft
+  , testCase "System.Directory.Extra.copyTemplate (valid template)"
+      testCopyTemplateOk
+  , testCase "System.Directory.Extra.copyTemplate (missing template dir)"
+      testCopyTemplateMissingDir
+  , testCase "System.Directory.Extra.copyTemplate (invalid encoding)"
+      testCopyTemplateInvalidEncoding
+  , testCase "System.Directory.Extra.copyTemplate (invalid syntax)"
+      testCopyTemplateInvalidSyntax
   ]
 
 -- * Data.List.Extra
@@ -89,3 +110,87 @@ propHeadEitherRight xs = not (null xs) ==> isRight (headEither xs)
 -- | Test that 'headEither' returns a 'Left' value if applied to an empty list.
 propHeadEitherLeft :: [Int] -> Property
 propHeadEitherLeft xs = null xs ==> isLeft (headEither xs)
+
+-- * System.Directory.Extra
+
+-- | Test that 'copyTemplate' expands a valid template, substituting the
+-- variables provided in the file contents.
+testCopyTemplateOk :: IO ()
+testCopyTemplateOk = do
+    (templateDir, targetDir) <- makeTestDirs "ok"
+
+    writeFile (templateDir </> "greeting.txt") "Hello, {{name}}!"
+
+    result <- try $ copyTemplate templateDir subst targetDir
+
+    contents <- readFile (targetDir </> "greeting.txt")
+
+    let testPass = isRight (result :: Either CopyTemplateError ())
+                     && contents == "Hello, World!"
+
+    assertBool "Expansion of a valid template failed" testPass
+  where
+    subst = object [ "name" .= ("World" :: String) ]
+
+-- | Test that 'copyTemplate' reports a list error when the template
+-- directory does not exist.
+testCopyTemplateMissingDir :: IO ()
+testCopyTemplateMissingDir = do
+    (templateDir, targetDir) <- makeTestDirs "missing"
+
+    removePathForcibly templateDir
+
+    result <- try $ copyTemplate templateDir (object []) targetDir
+
+    let testPass = case result of
+                     Left (CopyTemplateListError _ _) -> True
+                     _                                -> False
+
+    assertBool "Missing template directory not reported as a list error"
+               testPass
+
+-- | Test that 'copyTemplate' reports a decode error when a file in the
+-- template is not a valid UTF-8 text file.
+testCopyTemplateInvalidEncoding :: IO ()
+testCopyTemplateInvalidEncoding = do
+    (templateDir, targetDir) <- makeTestDirs "encoding"
+
+    -- 0xC3 followed by 0x28 is an invalid UTF-8 byte sequence.
+    BS.writeFile (templateDir </> "bad.txt") (BS.pack [0x68, 0xC3, 0x28])
+
+    result <- try $ copyTemplate templateDir (object []) targetDir
+
+    let testPass = case result of
+                     Left (CopyTemplateDecodeError _ _) -> True
+                     _                                  -> False
+
+    assertBool "Invalid UTF-8 file not reported as a decode error" testPass
+
+-- | Test that 'copyTemplate' reports a parse error when a file in the
+-- template contains invalid template syntax.
+testCopyTemplateInvalidSyntax :: IO ()
+testCopyTemplateInvalidSyntax = do
+    (templateDir, targetDir) <- makeTestDirs "syntax"
+
+    writeFile (templateDir </> "bad.txt") "{{#unclosed\nsome text\n"
+
+    result <- try $ copyTemplate templateDir (object []) targetDir
+
+    let testPass = case result of
+                     Left (CopyTemplateParseError _ _) -> True
+                     _                                 -> False
+
+    assertBool "Invalid template syntax not reported as a parse error"
+               testPass
+
+-- | Create empty template and target directories for a test with the given
+-- name, removing any leftovers from prior runs.
+makeTestDirs :: String -> IO (FilePath, FilePath)
+makeTestDirs name = do
+  tmpDir <- getTemporaryDirectory
+  let templateDir = tmpDir </> ("ogma-extra-tests-template-" ++ name)
+      targetDir   = tmpDir </> ("ogma-extra-tests-target-" ++ name)
+  removePathForcibly templateDir
+  removePathForcibly targetDir
+  createDirectoryIfMissing True templateDir
+  return (templateDir, targetDir)
