667e8420a5fe92f1204ffc9d1632414eb8096f4d
Ivan Perez <ivan.perezdominguez@nasa.gov>
2026-09-06 07:10:18 +0000

    ogma-core: Remove unused pragma. Refs #600.
    Several modules in `ogma-core` enable the language extension
    `OverloadedStrings` unnecessarily. For example, the file
    `ogma-core/src/Command/ROSApp.hs` enables the extension but compiles
    even if the extension is removed. Per our style guidelines, files should
    not contain unnecessary code.
    
    This commit removes the language pragma `OverloadedStrings` from any
    files in `ogma-core` that do not need it.

--- ogma-core/src/Command/Common.hs
+++ ogma-core/src/Command/Common.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE OverloadedStrings         #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
 -- Copyright 2022 United States Government as represented by the Administrator
 -- of the National Aeronautics and Space Administration. All Rights Reserved.
